1f9097c776f32b9641e586d9c566c91b2b10d2df
Fane Mo <500264@qq.com>
2019-03-22 18:50:00 +0800

    更新了单次批量升级程序upgrade_iTN185_331_multiprocess并打包成exe文件

--- upgrade_iTN185_331_multiprocess.py
+++ upgrade_iTN185_331_multiprocess.py
@@ -1,25 +1,25 @@
-'''ITN185_331批量升级工具v1.1说明
-编写人：莫凡 500264@qq.com
-鸣谢：冀文超 提供源代码思路
-版本日期：20190322
-说明：
-1.本脚本用于台式IPRAN设备的自动一键升级，用户不需检查待升级设备类型，脚本会自动搜索匹配升级规则，
+'''
+- ITN185_331批量升级工具v1.1说明
+- 编写人：莫凡 500264@qq.com
+- 鸣谢：冀文超 提供源代码思路
+- 版本日期：20190322
+- 说明：
+1. 本脚本用于台式IPRAN设备的自动一键升级，用户不需检查待升级设备类型，脚本会自动搜索匹配升级规则，
 如果匹配不到规则，会报错并继续匹配下一台;
-2.升级规则通过/upgrade_rule.csv定制。包括匹配设备类型、硬件版本、指定目标bootrom版本和文件、
+2. 升级规则通过/upgrade_rule.csv定制。包括匹配设备类型、硬件版本、指定目标bootrom版本和文件、
 指定目标system版本和文件、是否备份配置、ftp下载账号、是否重启激活等。每种设备一行，详见具体文件。
-3.理论上也适用于其他iTN1xx，2xx系列台式终端的升级，只要命令行格式和回显风格一致即可，请读者自行验证。
-4.升级时一定要保证itn设备FLASH内存空间足够，否则异常!
-5.升级设备ip列表放在/u_ip_list.txt供程序读取设备ip，每行一条ip
-6.升级结果日志放在/result子目录下，文件名为“日期_时间.csv”，如果/result目录不存在，自动创建
-7.程序为多进程并行升级，需根据PC性能配置决定并发进程池大小Pool（），数量过多会影响稳定性，ftp服务端
+3. upgrade_rule.csv、u_ip_list.txt必须和程序文件放在同一目录下，且不能改名。
+4. 升级时一定要保证itn设备FLASH内存空间足够，否则异常!
+5. 升级设备ip列表放在/u_ip_list.txt供程序读取设备ip，每行一条ip
+6. 升级结果日志放在/result子目录下，文件名为“日期_时间.csv”，如果/result目录不存在，自动创建
+7. 程序为多进程并行升级，需根据PC性能配置决定并发进程池大小Pool（），数量过多会影响稳定性，ftp服务端
 程序需要支持高并发下载，推荐使用FileZilla server服务器程序。
-8.升级规则可以指定升级前、升级成功后擦除旧版本文件，当然新版本文件名不能用同样的名字，否则会被误擦除！
-9.upgrade_rule.csv、u_ip_list.txt必须和程序文件放在同一目录下，且不能改名。
+8. 升级规则可以指定升级前、升级成功后擦除旧版本文件，当然新版本文件名不能用同样的名字，否则会被误擦除！
 '''
 
 import telnetlib  # 调用telnet方法需要的库
 from datetime import datetime
-from multiprocessing import Pool
+from multiprocessing import Pool, freeze_support
 from time import sleep
 import os
 
@@ -293,7 +293,6 @@ def multiprocess_update(p_num):
     f3 = os.path.join(home, 'result')
     if not os.path.exists(f3):
         os.makedirs(f3)
-    print(f3)
     start_time = datetime.now()
     # 结果日志存放路径，文件名为格式化日期_时间.csv
     logfile = os.path.join(f3, '%s.csv') % start_time.strftime('%Y%m%d_%H%M%S')
@@ -317,7 +316,7 @@ def multiprocess_update(p_num):
 
 if __name__ == '__main__':
     # windows的可执行文件，必须添加支持程序冻结，该命令需要在__main__函数下
-    # freeze_support()
+    freeze_support()
     while True:
         # 接收控制台输入，input方法获取的是字符串，需要转成整数
         p_num = int(input('请输入并发进程数，根据PC处理能力适当选择，推荐为CPU数量的整数倍：'))
@@ -325,3 +324,4 @@ if __name__ == '__main__':
             break
         print("非法数值，请从新输入1到99之间的整数")
     multiprocess_update(p_num)
+    input('升级完成，升级结果日志放在/result子目录下，文件名为“日期_时间.csv”，按回车退出')
